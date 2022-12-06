8e77f4e3266072735c555ca9dddbcb6e208e76c0
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-06 11:27:02 -0300

    feat(specifications): create specifications routes

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository'
+import { CreateSpecificationUseCase } from '../modules/cars/useCases/CreateSpecificationUseCase'
+
+export const specificationRoutes = Router()
+
+const specificationsRepository = new SpecificationsRepository()
+
+specificationRoutes.post('/', (req, res) => {
+  const { name, description } = req.body
+
+  const createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepository)
+
+  createSpecificationUseCase.execute({ name, description })
+
+  return res.status(201).send()
+})
