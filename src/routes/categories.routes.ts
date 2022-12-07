fbccb6ea98425c3268242571e103cc1767413ea6
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-07 10:56:28 -0300

    feat(multer): create multer setup and middleware to import categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import multer from 'multer'
 import { createCategoryFactory } from '../modules/cars/useCases/createCategory/CreateCategoryFactory'
 import { listCategoriesFactory } from '../modules/cars/useCases/listCategories/ListCategoriesFactory'
 
 export const categoriesRoutes = Router()
 
+const upload = multer({
+  dest: './tmp',
+})
+
 categoriesRoutes.post('/', (req, res) => createCategoryFactory().handle(req, res))
 
 categoriesRoutes.get('/', (req, res) => listCategoriesFactory().handle(req, res))
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  const { file } = req
+
+  console.log(file)
+
+  return res.send()
+})
