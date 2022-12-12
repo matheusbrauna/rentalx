8fac25d33e98f73151221ac1206527d2b91b6dc9
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-12 11:10:19 -0300

    chore(swagger): add swagger-ui-express setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
 import { router } from './routes'
+import swaggerFile from './swagger.json'
 
 import 'dotenv/config'
 
 export const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
